3cd6dba518261019adf4e4972d24911f6ef45316
JAT <jat1sc@icloud.com>
2013-07-09 19:04:09 -0400

    Corrected errors in web.js

--- web.js
+++ web.js
@@ -10,9 +10,9 @@ app.get('/', function(request, response) {
 
     buf = fs.readFileSync("index.html");
     var output = buf.toString;
-    
-    response.send(output);
 
+    response.send(output);
+    });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
