2c90280af4f3404dbac8c84f896d41e69fb38a74
JAT <jat1sc@icloud.com>
2013-07-10 18:25:17 -0400

    Added url input capability

--- grader.js
+++ grader.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 /*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio.  Teaches command line application development
+Automatically grade files for the presence of specified HTML tags/attributes.  Uses commander.js and cheerio.  Teaches command line application development
 and basic DOM parsing.
 
 References:
@@ -25,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -32,7 +32,7 @@ var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
 	console.log("%s does not exist. Exiting.", instr);
-	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	process.exit(1);
     }
     return instr;
 };
@@ -41,20 +41,28 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(htmlData, checksfile) {
+    console.log("in checkHTML");
+    $=cheerio.load(htmlData);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
 	var present = $(checks[ii]).length > 0;
 	out[checks[ii]] = present;
     }
-    return out;
+    console.log("checkHtml returns out = "+out);
+    outJson = JSON.stringify(out,null,4);
+    console.log("outjson = "+outJson);
+
 };
+
+
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -62,16 +70,41 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
+var buildFn = function(checksfile) {
+
+    var processURL = function(result, response) {
+	console.log("Result = "+result);
+	console.log("Response = "+response);
+	checkHtml(result,checksfile);
+    };
+    return processURL;
+};
+
+
+
+
 if(require.main == module) {
     program
        .option('-c, --checks <check_file>', 'path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+       .option('-u, --url <url>', 'URL to check')
        .parse(process.argv);
 
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+ 
+     processURL = buildFn(program.checks);
+ 
+    if(program.url) {
+	console.log("URL entered");
+        rest.get("http://vast-shore-3806.herokuapp.com").on('complete',clone(processURL));
+	}
+    else {
+	console.log("No URL entered");
+	checkHtml(fs.readFileSync(program.file),program.checks);
+	}
 }
 else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
+
